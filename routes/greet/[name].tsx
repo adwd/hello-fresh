14452f7b40c1d383bd2b00c7d1b7adec8eace332
adwd <ecdt.mn@gmail.com>
2023-11-27 21:22:32 +0900

    initialize

---
+++ routes/greet/[name].tsx
@@ -0,0 +1,39 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+const NAMES = ["Alice", "Bob", "Charlie", "Dave", "Eve", "Frank"];
+
+interface Data {
+  results: string[];
+  query: string;
+}
+
+export const handler: Handlers<Data> = {
+  GET(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+    const results = NAMES.filter((name) => name.includes(query));
+    return ctx.render({ results, query });
+  },
+  POST(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+    const results = NAMES.filter((name) => name.includes(query));
+    return ctx.render({ results, query });
+  },
+};
+
+export default function Page({ data, params }: PageProps<Data>) {
+  const { results, query } = data;
+  return (
+    <div>
+      <div>Hello {params.name}</div>
+      <form method="post">
+        <input type="text" name="q" value={query} />
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {results.map((name) => <li key={name}>{name}</li>)}
+      </ul>
+    </div>
+  );
+}
